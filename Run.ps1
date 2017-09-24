84be9c56cae585668a3df628ceaddb212ff77e71
Dominique Broeglin <dominique.broeglin@gmail.com>
2017-09-24 15:39:13 +0200

    Sovled VMWare shared folder issue (if -WithCopy is provided to Run.ps1, first copy from z: to c:)

--- Run.ps1
+++ Run.ps1
@@ -1,5 +1,10 @@
 [CmdletBinding()]
 Param(
+    [String]
+    $LanguageList = "fr-CH",
+
+    [Switch]
+    $WithCopy
 )
 
 $RequiredModules = "xPSDesiredStateConfiguration"
@@ -7,12 +12,29 @@ $RequiredModules = "xPSDesiredStateConfiguration"
 
 $ErrorActionPreference = 'Stop'
 $PSPathSeparator       = [System.IO.Path]::PathSeparator
-$LocalModulesDir       = Join-Path $PSScriptRoot "Modules"
-$LocalModulesCacheDir  = Join-Path $PSScriptRoot "ModulesCache"
+$resetLcm              = $False
+
+if ($WithCopy) {
+    $Root = "c:\dsc-dev"
+    Write-Verbose "Using $Root as working directory..."
+    if (Test-Path $Root) {
+        Remove-Item -Recurse -Force $Root        
+    }
+    Copy-Item -Recurse -Force $PSScriptRoot $Root
+    $LocalModulesCacheDir  = "C:\ModulesCache"
+} else {
+    $Root = $PSScriptRoot
+    $LocalModulesCacheDir  = Join-Path $Root "ModulesCache"
+}
+
+$LocalModulesDir       = Join-Path $Root "Modules"
+
+Write-Verbose "Setting language list to $LanguageList..."
+Set-WinUserLanguageList -LanguageList $LanguageList -Force
 
 if (-not (Get-PackageProvider -name NuGet)) {
     Write-Verbose "Installing Nuget package provider..."
-    Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 }
 if (-not (Get-PSRepository PSGallery ).Trusted) {
     Write-Verbose "Setting PSGallery installation policy to Trusted..."
@@ -25,27 +47,54 @@ if ("Stopped" -eq (Get-Service -Name winrm).Status) {
 }
 
 if (-not ($env:PSModulePath -split $PSPathSeparator).Contains($LocalModulesDir)) {
+    Write-Verbose "Adding $LocalModulesDir to PSMODULEPATH"
     $env:PSModulePath = "$LocalModulesDir$PSPathSeparator$env:PSModulePath"
+    [environment]::SetEnvironmentVariable("PSModulePath", $env:PSModulePath, "Machine")
+    $resetLcm = $True
 }
 
 if (-not ($env:PSModulePath -split $PSPathSeparator).Contains($LocalModulesCacheDir)) {
+    Write-Verbose "Adding $LocalModulesCacheDir to PSMODULEPATH"
     $env:PSModulePath = "$LocalModulesCacheDir$PSPathSeparator$env:PSModulePath"
+    [environment]::SetEnvironmentVariable("PSModulePath", $env:PSModulePath, "Machine")
+    $resetLcm = $True
 }
 
+if ($resetLcm) {
+    Write-Verbose "Reseting the LCM..."
+    $dscProcessID = Get-WmiObject msft_providers | 
+    Where-Object {$_.provider -like 'dsccore'} | 
+    Select-Object -ExpandProperty HostProcessIdentifier
+    if ($dscProcessID) {
+        Write-Verbose "Killing process $dscProcessId..."
+        Get-Process -Id $dscProcessID | Stop-Process -Force          
+    }
+}
+
+if (-not (Test-Path -Path $LocalModulesCacheDir)) {
+    New-Item -ItemType Container -Path $LocalModulesCacheDir > $Null
+}
 $RequiredModules | ForEach-Object {
     if (-not (Get-Module -ListAvailable $_)) {
+        Write-Verbose "Installing module $_ to $LocalModulesCacheDir..."
         Save-Module $_ -Path $LocalModulesCacheDir
     }
 }
 
-. $PSScriptRoot\DevConfig.ps1
+Push-Location
+Set-Location $Root
+try { 
+    . $Root\DevConfig.ps1
 
-DevConfig -ConfigurationData @{ 
-    AllNodes = @(
-        @{
-            NodeName = $env:COMPUTERNAME
-        }
-    )} -OutputPath DevConfig -Verbose
+    DevConfig -ConfigurationData @{ 
+        AllNodes = @(
+            @{
+                NodeName = $env:COMPUTERNAME
+            }
+        )} -OutputPath DevConfig -Verbose
 
-Set-DscLocalConfigurationManager -Path DevConfig -Verbose
-Start-DscConfiguration -Wait  -Force -Path DevConfig -Verbose -Debug
+    Set-DscLocalConfigurationManager -Path DevConfig -Verbose
+    Start-DscConfiguration -Wait  -Force -Path DevConfig -Verbose -Debug
+} finally {
+    Pop-Location
+}
